411a3a74b946fe0d5ee500e3ab3e42cbb9ba7295
Victor Gaston Morales Scaro <gastymorales113@gmail.com>
2020-09-19 17:44:24 -0300

    Fix little errors

--- src/profile/containers/Profile.js
+++ src/profile/containers/Profile.js
@@ -3,8 +3,7 @@ import {View, Text, StyleSheet, SafeAreaView, ScrollView} from 'react-native';
 import {connect} from 'react-redux';
 import Container from '../../generales/Container'
 import { ProfileMainView } from '../components/ProfileMainView'
-import { MenuList } from '../../generales/MenuList'
-import { menu } from '../../constantes/Menu'
+import { ProfileMenuItem } from '../components/ProfileMenuItem'
 import { colores } from '../../constantes/Temas';
 
 function Profile(props) {
@@ -13,10 +12,23 @@ function Profile(props) {
       <Container>
                 <View style={{width: '100%', height: '100%', alignItems: 'center'}}>
                     <ProfileMainView />
-                    <MenuList list={menu} />
-                    <MenuList list={[{name: 'Cerrar sesión', color: colores.rojo}]} />  
+                    <View style={styles.menuContainer}>
+                        <View style={{width: '100%', paddingHorizontal: '5%', paddingVertical: '5%'}}>
+                          <ProfileMenuItem name='Mis Compras' icon='shopping-basket' color='#f18263' />
+                          <ProfileMenuItem name='Mis Borradores' icon='edit' color='#50c8ff' />
+                          <ProfileMenuItem name='Mis Albunes compartidos' icon='insert-photo' color='#5d58e0' />
+                          <ProfileMenuItem name='Mis repositorios' icon='folder' color='#e0bb2e' />
+                          <ProfileMenuItem name='Politicas de Privacidad' icon='policy' color='#ffd948' />
+                          <ProfileMenuItem name='Somos #Weprint' icon='supervisor-account' color='#ffd948' divider={false} />
+                        </View>
+                    </View>
+                    <View style={styles.menuContainer}>
+                        <View style={{width: '100%', paddingHorizontal: '5%', paddingVertical: '5%'}}>                          
+                          <ProfileMenuItem name='Cerrar Sesion' color='#ffd948' divider={false} />
+                        </View>
+                    </View>
                 </View>
-      </Container>
+      </Container>  
   );
 }
 
@@ -27,6 +39,14 @@ const styles = StyleSheet.create({
     width: '100%',
     flex: 1,
   },
+  menuContainer: {
+    alignItems:'center', 
+    width: '90%', 
+    paddingVertical: 5, 
+    paddingHorizontal: 10, 
+    backgroundColor: colores.blanco,
+    marginTop: 10,
+  }
 });
 
 const mapStateToProps = (state) => ({login: state.login});
