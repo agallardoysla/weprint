c941695584a9b49e96e99284acb8b18eff8c90b5
Wilmer <wilmer.payall@gmail.com>
2020-11-10 05:45:12 -0400

    resolve bugfix formats

--- src/home/components/ProjectDraft.js
+++ src/home/components/ProjectDraft.js
@@ -16,7 +16,6 @@ const ProjectDraft = ({cart, onPressGoToDetail}) => {
   return (
     <TouchableOpacity
       style={{...style.mainContainer, width: useWindowDimensions().width - 25}}
-      delayPressIn={0}
       onPress={handleOnPress}>
       <View style={style.imageContainer}>
         <Image
